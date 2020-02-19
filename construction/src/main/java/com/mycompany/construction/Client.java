0ce7f5ab5182ad89b44863692fb665a58ac9c1d4
nikhita518 <59025241+nikhita518@users.noreply.github.com>
2020-02-19 16:16:23 +0530

    updated file

--- construction/src/main/java/com/mycompany/construction/Client.java
+++ construction/src/main/java/com/mycompany/construction/Client.java
@@ -1,13 +1,33 @@
 package com.mycompany.construction;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class Client 
 {
+	private int house;
+	private int standard;
+	private String automated;
+	
+	public Client()
+	{
+		Scanner scan = new Scanner(System.in);
+		PrintStream print = new PrintStream(System.out);
+		print.print("Enter the area of the house in square feet:");
+		house = scan.nextInt();
+		print.print("1.Use standard materials\n2.Use above standard materials\n3.Use high standard materials\nEnter the standard you need for your house:");
+		standard = scan.nextInt();
+		print.print("Enter do you need your house automated (y/n)");
+		automated = scan.next();
+		
+		print.print(new HouseConstruction().estimationCost(house,standard,automated));
+	}
     public static void main( String[] args )
     {
-        HouseConstruction house = new HouseConstruction();
+        new Client();
     }
 }
--- construction/src/main/java/com/mycompany/construction/HouseConstruction.java
+++ construction/src/main/java/com/mycompany/construction/HouseConstruction.java
@@ -1,43 +1,23 @@
 package com.mycompany.construction;
 
-import java.io.PrintStream;
-import java.util.Scanner;
-
 public class HouseConstruction 
 {
-	private int house;
-	private int standard;
-	private String automated;
-	public HouseConstruction()
-	{
-		Scanner scan = new Scanner(System.in);
-		PrintStream print = new PrintStream(System.out);
-		print.print("Enter the area of the house in square feet:");
-		house = scan.nextInt();
-		print.print("1.Use standard materials\n2.Use above standard materials\n3.Use high standard materials\nEnter the standard you need for your house:");
-		standard = scan.nextInt();
-		print.print("Enter do you need your house automated (yes/no)");
-		automated = scan.next();
-		
-		estimationCost();
-	}
 	
-	public void estimationCost()
+	public int estimationCost(int house, int standard, String automated)
 	{
-		PrintStream print = new PrintStream(System.out);
-		if(automated == "yes")
+		if(automated.equals("y") && standard == 3)
 		{
-			print.print(house*2500);
+			return house*2500;
 		}
 		else if(standard == 3)
 		{
-			print.print(house*1800);
+			return house*1800;
 		}
 		else if(standard == 2)
 		{
-			print.print(house*1500);
+			return house*1500;
 		}
 		else
-			print.print(house*1200);
+			return house*1200;
 	}
 }
